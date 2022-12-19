2a3db7f00f29c8f5c3867897b198e1d6ddb3453c
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-19 23:01:04 +0900

    feature : mongoDB set.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,10 +7,27 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+// mongodb 연결
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useMongoClient: true,
+  })
+  .then((response) => {
+    console.log('Successfully connected to mongodb');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('heurm server is listening to port 4000');
+app.listen(port, () => {
+  console.log('heurm server is listening to port ' + port);
 });
