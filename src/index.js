845231165cd89c7ef095978960b9deafcbd66bf7
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-19 22:07:17 +0900

    feature : api set.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+const api = require('./api');
 
-app.use((ctx, next) => {
-  console.log(1);
-  next();
-});
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use(async (ctx, next) => {
-  console.log(1);
-  const started = new Date();
-  await next();
-  console.log(new Date() - started + 'ms');
-});
-
-app.use((ctx) => {
-  ctx.body = 'Hello Koa';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('heurm server is listening to port 4000');
