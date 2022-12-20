593bd92449c8b40e11027d1b8403f0a8bb01c83a
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-21 08:37:33 +0900

    feature : git page set.

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -6,17 +6,17 @@ const handler = (ctx, next) => {
   ctx.body = `${ctx.request.method} ${ctx.request.path}`;
 };
 
-books.get('/', handler);
-books.post('/', handler);
-books.delete('/', handler);
-books.put('/', handler);
-books.patch('/', handler);
+// books.get('/', handler);
+// books.post('/', handler);
+// books.delete('/', handler);
+// books.put('/', handler);
+// books.patch('/', handler);
 
-// const booksCtrl = require('./books.controller');
-// books.get('/', booksCtrl.list);
-// books.post('/', booksCtrl.create);
-// books.delete('/', booksCtrl.delete);
-// books.put('/', booksCtrl.replace);
-// books.patch('/', booksCtrl.update);
+const booksCtrl = require('./books.controller');
+books.get('/', booksCtrl.list);
+books.post('/', booksCtrl.create);
+books.delete('/', booksCtrl.delete);
+books.put('/', booksCtrl.replace);
+books.patch('/', booksCtrl.update);
 
 module.exports = books;
