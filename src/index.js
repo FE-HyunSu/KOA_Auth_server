f9043f2ae2ece9b3ea338f887e45045ff6f1002d
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-20 22:30:41 +0900

    feature : server update.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = '홈';
+});
+
+app.use((ctx, next) => {
+  console.log(ctx);
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log(1);
+  const started = new Date();
+  next().then(() => {
+    console.log(new Date() - started + 'ms');
+  });
+});
 
 app.use((ctx) => {
   ctx.body = 'Hello Koa';
@@ -8,37 +28,3 @@ app.use((ctx) => {
 app.listen(4000, () => {
   console.log('heurm server is listening to port 4000');
 });
-
-// require('dotenv').config(); // .env 파일에서 환경변수 불러오기
-
-// const Koa = require('koa');
-// const Router = require('koa-router');
-
-// const app = new Koa();
-// const router = new Router();
-// const api = require('./api');
-
-// const mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
-// // mongodb 연결
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useMongoClient: true,
-//   })
-//   .then((response) => {
-//     console.log('Successfully connected to mongodb');
-//   })
-//   .catch((e) => {
-//     console.error(e);
-//   });
-
-// const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
-
-// router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
-
-// app.use(router.routes()).use(router.allowedMethods());
-
-// app.listen(port, () => {
-//   console.log('heurm server is listening to port ' + port);
-// });
